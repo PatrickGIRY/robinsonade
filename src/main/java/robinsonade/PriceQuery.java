7b4212d5159446ee82ca1ff363985c806b071e16
Patrick GIRY <patrickgiry@gmail.com>
2017-06-13 08:41:27 +0200

    refactor(robinsonade) : use reduce instead of for loop

--- src/main/java/robinsonade/PriceQuery.java
+++ src/main/java/robinsonade/PriceQuery.java
@@ -1,5 +1,8 @@
 package robinsonade;
 
+import java.util.Arrays;
+import java.util.function.BiFunction;
+
 class PriceQuery {
 
     private final ItemReference[] itemReferences;
@@ -9,11 +12,21 @@ class PriceQuery {
     }
 
     Result findPrice(String soughtItemCode) {
-        for (ItemReference itemReference : itemReferences) {
+        return reduce(Result.notFound(soughtItemCode),
+                (result, itemReference) -> {
             if (itemReference.matchSoughtItemCode(soughtItemCode)) {
                 return Result.found(itemReference.getUnitPrice());
+            } else {
+                return result;
             }
+        }, Arrays.asList(itemReferences));
+    }
+
+    static <T, R> R reduce(R identity, BiFunction<R, T, R> reducer, Iterable<T> values) {
+        R result = identity;
+        for (T value : values) {
+            result = reducer.apply(result, value);
         }
-        return Result.notFound(soughtItemCode);
+        return result;
     }
 }
