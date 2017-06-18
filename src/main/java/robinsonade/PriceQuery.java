01dddfe2a3b96f5fed25d6511e5cab0f3d88a7c8
Patrick GIRY <patrickgiry@gmail.com>
2017-06-18 02:20:55 +0200

    refactor(robinsonade) : use stream

--- src/main/java/robinsonade/PriceQuery.java
+++ src/main/java/robinsonade/PriceQuery.java
@@ -1,10 +1,6 @@
 package robinsonade;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.BiFunction;
-import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 class PriceQuery {
 
@@ -15,27 +11,13 @@ class PriceQuery {
     }
 
     Result findPrice(String soughtItemCode) {
-        return reduce(Result.notFound(soughtItemCode),
-                (result, itemReference) -> Result.found(itemReference.getUnitPrice()),
-                filter(itemReference -> itemReference.matchSoughtItemCode(soughtItemCode), Arrays.asList(itemReferences)));
+
+        return Stream.of(itemReferences)
+                .filter(r -> r.matchSoughtItemCode(soughtItemCode))
+                .map(ItemReference::getUnitPrice)
+                .findFirst()
+                .map(Result::found)
+                .orElseGet(() -> Result.notFound(soughtItemCode));
     }
 
-    private static <T> List<T> filter(Predicate<T> predicate, Iterable<T> values) {
-        return reduce(new ArrayList<>(), (accumulator, value) -> predicate.test(value) ? append(accumulator, value) : accumulator,  values);
-    }
-    
-
-    private static <T extends List<R>, R> T append(T accumulator, R value) {
-        accumulator.add(value);
-        return accumulator;
-    }
-
-
-    private static <T, R> R reduce(R identity, BiFunction<R, T, R> reducer, Iterable<T> values) {
-        R result = identity;
-        for (T value : values) {
-            result = reducer.apply(result, value);
-        }
-        return result;
-    }
 }
