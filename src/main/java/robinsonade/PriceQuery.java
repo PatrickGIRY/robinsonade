cda4329fe7ad06de1104ab5c758bfd1f7d31b5cd
Patrick GIRY <patrickgiry@gmail.com>
2017-06-18 02:06:59 +0200

    refactor(robinsonade) : use filter and reduce

--- src/main/java/robinsonade/PriceQuery.java
+++ src/main/java/robinsonade/PriceQuery.java
@@ -1,7 +1,10 @@
 package robinsonade;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.BiFunction;
+import java.util.function.Predicate;
 
 class PriceQuery {
 
@@ -13,16 +16,22 @@ class PriceQuery {
 
     Result findPrice(String soughtItemCode) {
         return reduce(Result.notFound(soughtItemCode),
-                (result, itemReference) -> {
-            if (itemReference.matchSoughtItemCode(soughtItemCode)) {
-                return Result.found(itemReference.getUnitPrice());
-            } else {
-                return result;
-            }
-        }, Arrays.asList(itemReferences));
+                (result, itemReference) -> Result.found(itemReference.getUnitPrice()),
+                filter(itemReference -> itemReference.matchSoughtItemCode(soughtItemCode), Arrays.asList(itemReferences)));
     }
 
-    static <T, R> R reduce(R identity, BiFunction<R, T, R> reducer, Iterable<T> values) {
+    private static <T> List<T> filter(Predicate<T> predicate, Iterable<T> values) {
+        return reduce(new ArrayList<>(), (accumulator, value) -> predicate.test(value) ? append(accumulator, value) : accumulator,  values);
+    }
+    
+
+    private static <T extends List<R>, R> T append(T accumulator, R value) {
+        accumulator.add(value);
+        return accumulator;
+    }
+
+
+    private static <T, R> R reduce(R identity, BiFunction<R, T, R> reducer, Iterable<T> values) {
         R result = identity;
         for (T value : values) {
             result = reducer.apply(result, value);
