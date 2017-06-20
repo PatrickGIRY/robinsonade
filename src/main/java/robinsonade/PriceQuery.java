ce937a2464f0bfa89987ded6da9b947c98adf8ca
Patrick GIRY <patrickgiry@gmail.com>
2017-06-20 13:42:57 +0200

    refactor(robinsonade) : build map with stream

--- src/main/java/robinsonade/PriceQuery.java
+++ src/main/java/robinsonade/PriceQuery.java
@@ -1,23 +1,20 @@
 package robinsonade;
 
+import java.util.Map;
 import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toMap;
 
 class PriceQuery {
 
-    private final ItemReference[] itemReferences;
+    private final Map<String, Price> itemReferencesByItemCode;
 
     public PriceQuery(ItemReference... itemReferences) {
-        this.itemReferences = itemReferences;
+        this.itemReferencesByItemCode = Stream.of(itemReferences).collect(toMap(ItemReference::getItemCode, ItemReference::getUnitPrice));
     }
 
     Result findPrice(String soughtItemCode) {
-
-        return Stream.of(itemReferences)
-                .filter(r -> r.matchSoughtItemCode(soughtItemCode))
-                .map(ItemReference::getUnitPrice)
-                .map(Result::found)
-                .findFirst()
-                .orElseGet(() -> Result.notFound(soughtItemCode));
+        Price price = itemReferencesByItemCode.get(soughtItemCode);
+        return price != null ? Result.found(price) : Result.notFound(soughtItemCode);
     }
-
 }
