6fb173662a0c062d3f18fab5efeefe19daeb4b54
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2019-01-20 20:28:06 +0100

    refactor(robinsonade) : Add Price value type

--- CSharp/Robinsonade.Tests/CashRegisterTest.cs
+++ CSharp/Robinsonade.Tests/CashRegisterTest.cs
@@ -17,10 +17,10 @@ namespace Robinsonade.Tests
         [TestMethod]
         public void total_is_product_of_quantity_by_item_price()
         {
-            double price = 1.20;
+            Price price = Price.ValueOf(1.20);
             double quantity = 1;
-            double total = cashRegister.Total(price, quantity);
-            Check.That(total).IsEqualTo(1.20);
+            Price total = cashRegister.Total(price, quantity);
+            Check.That(total).IsEqualTo(Price.ValueOf(1.20));
         }
     }
 }
--- CSharp/Robinsonade/CashRegister.cs
+++ CSharp/Robinsonade/CashRegister.cs
@@ -2,9 +2,9 @@
 {
     public class CashRegister
     {
-        public double Total(double price, double quantity)
+        public Price Total(Price price, double quantity)
         {
-            return price * quantity;
+            return price.MultiplyBy(quantity);
         }
     }
 }
