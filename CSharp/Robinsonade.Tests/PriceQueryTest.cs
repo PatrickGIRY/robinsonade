c552bcb4c195c0404dc7ec6cd2403b2972fb731c
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2019-01-20 20:36:41 +0100

    feat(robinsonade) : Add query to find price given an item code

---
+++ CSharp/Robinsonade.Tests/PriceQueryTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NFluent;
+
+namespace Robinsonade.Tests
+{
+    [TestClass]
+    public class PriceQueryTest
+    {
+        private PriceQuery priceQuery;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            priceQuery = new PriceQuery();
+        }
+
+        [TestMethod]
+        public void find_the_price_given_an_item_code()
+        {
+            Check.That(priceQuery.FindPrice("APPLE")).IsEqualTo(Price.ValueOf(1.20));
+        }
+    }
+}
---
+++ CSharp/Robinsonade/PriceQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Robinsonade
+{
+    public class PriceQuery
+    {
+        public Price FindPrice(String itemCode)
+        {
+            return Price.ValueOf(1.20);
+        }
+    }
+}
